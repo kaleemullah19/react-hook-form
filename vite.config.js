350756552e7df3f869b21ee87c93b197ff2bf80c
kaleemullah khan <kaleemullah19@gmail.com>
2025-03-16 18:06:09 -0400

    deployment fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["yup"],
+      external: ["yup"], // Add 'yup' here if needed
     },
   },
 });
